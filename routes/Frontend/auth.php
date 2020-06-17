08d6a051aa451cf6d8127f7d64ff165ca7d4b9c4
ruchit <ruchit.patel@viitor.clooud>
2020-06-17 15:53:34 +0530

    Update Frontend route path

--- routes/Frontend/auth.php
+++ routes/Frontend/auth.php
@@ -4,9 +4,9 @@ Auth::routes(['name' => 'frontend.','verify' => true]);
 Route::group(['namespace' => 'Auth'], function () {
     Route::get('login/{provider}', 'SocialAccountController@redirectToProvider')->name('frontend.social-media.login');
     Route::get('login/{provider}/callback', 'SocialAccountController@handleProviderCallback');
-
-    Route::get('email-verification/{email}/{verification_code}', 'UserController@emailVerification')->name('frontend.email-verification');
 });
+
+Route::get('email-verification/{email}/{verification_code}', 'UserController@emailVerification')->name('frontend.email-verification');
 
 /*----------------------------- AUTHENTICATION ROUTES ENDS-----------------------------*/
 
